fb55e4ee874983c0b29201ef6a84e5a4c133217d
Ben Harter-Murphy <benhartermurphy@gmail.com>
2022-05-02 17:54:59 -0400

    changes

--- first-tts.py
+++ first-tts.py
@@ -5,6 +5,9 @@ name = input("What is your name? ")
 voices = engine.getProperty("voices")
 engine.setProperty("voice", voices[7].id)
 engine.setProperty("rate", 178)
-engine.say(f"Hello, {name}, I hope you feel better!")
+engine.say(f"Hello, {name}, I'm so sorry that you had a rough day, fuck them shitty parents!")
 engine.runAndWait()
 engine.stop()
+
+# run python3 first-tts.py
+# then type in name 
